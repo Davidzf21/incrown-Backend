0677abd697c58fd2c6007f958128459ce4ec549c
Davidzf21 <zandundo2a@gmail.com>
2022-11-22 11:45:15 +0100

    Mejora test

--- incrown_app/tests.py
+++ incrown_app/tests.py
@@ -8,7 +8,7 @@ url_web = 'https://incrown.onrender.com/incrown_app/'
 # TEST USUARIOS
 def test_usuarios(url):
     print("-----------------------------------------------------\nTEST: USUARIOS")
-    #Creacion
+    #Creacion Buena
     auth_data = {'nombre': 'prueba', 'username': 'prueba', 'correo': 'prueba@gmail.com', 'password': '1234'}
     requests.post(url + 'CreateUsuario/', data=auth_data)
     resp = requests.get(url + 'Usuario/prueba')
@@ -17,7 +17,15 @@ def test_usuarios(url):
         print("\t -> CREACION DE UN USUARIO --> FUNCIONA")
     else:
         print("\t -> CREACION DE UN USUARIO --> NO FUNCIONA")
-    #Modificacion
+    #Creacion Mala
+    auth_data = {'nombre': 'prueba', 'username': 'prueba', 'correo': 'prueba@gmail.com', 'password': '1234'}
+    resp = requests.post(url + 'CreateUsuario/', data=auth_data)
+    t = resp.json()
+    if "Username ya existe" in t['message']:
+        print("\t -> NO CREACION DE UN USUARIO YA EXISTENTE--> FUNCIONA")
+    else:
+        print("\t -> NO CREACION DE UN USUARIO YA EXISTENTE --> NO FUNCIONA")
+    # Modificacion Buena
     auth_data = {'nombre': 'prueba1', 'username': 'prueba', 'correo': 'prueba1@gmail.com', 'password': '12345'}
     requests.put(url + 'UpdateUsuario/prueba/', data=auth_data)
     resp = requests.get(url + 'Usuario/prueba')
@@ -26,7 +34,28 @@ def test_usuarios(url):
         print("\t -> MODIFICACION DE UN USUARIO --> FUNCIONA")
     else:
         print("\t -> MODIFICACION DE UN USUARIO --> NO FUNCIONA")
-    #Eliminacion
+    # Modificacion Mala
+    auth_data = {'nombre': 'prueba1', 'username': 'prueba', 'correo': 'prueba1@gmail.com', 'password': '12345'}
+    resp = requests.put(url + 'UpdateUsuario/prueba1/', data=auth_data)
+    if (resp.status_code == 500):
+        print("\t -> NO MODIFICACION DE UN USUARIO NO EXISTENTE --> FUNCIONA")
+    else:
+        print("\t -> NO MODIFICACION DE UN USUARIO NO EXISTENTE --> NO FUNCIONA")
+    # Login con contraseña Correcta
+    resp = requests.get(url + 'Login/prueba/12345')
+    t = resp.json()
+    if ("Contraseña correcta" in t['message']):
+        print("\t -> LOGIN DE CONTRASEÑA CORRECTA --> FUNCIONA")
+    else:
+        print("\t -> LOGIN DE CONTRASEÑA CORRECTA --> NO FUNCIONA")
+    # Login con contraseña Incorrecta
+    resp = requests.get(url + 'Login/prueba/123455')
+    t = resp.json()
+    if ("ERROR: Contraseña incorrecta" in t['message']):
+        print("\t -> LOGIN DE CONTRASEÑA INCORRECTA --> FUNCIONA")
+    else:
+        print("\t -> LOGIN DE CONTRASEÑA INCORRECTA --> NO FUNCIONA")
+    # Eliminacion Buena
     requests.delete(url + 'DeleteUsuario/prueba')
     resp = requests.get(url + 'Usuario/prueba')
     t = resp.json()
@@ -34,12 +63,19 @@ def test_usuarios(url):
         print("\t -> ELIMINACION DE UN USUARIO --> FUNCIONA")
     else:
         print("\t -> ELIMINACION DE UN USUARIO --> NO FUNCIONA")
+    # Eliminacion Mala
+    resp = requests.delete(url + 'DeleteUsuario/prueba1')
+    t = resp.json()
+    if "Not found." in t['detail']:
+        print("\t -> ELIMINACION DE UN USUARIO NO EXISTENTE --> FUNCIONA")
+    else:
+        print("\t -> ELIMINACION DE UN USUARIO NO EXISTENTE --> NO FUNCIONA")
     print("-----------------------------------------------------\n")
 
 # TEST EVENTOS
 def test_eventos(url):
     print("-----------------------------------------------------\nTEST: EVENTOS")
-    #Creacion
+    # Creacion Buena
     auth_data_us = {'nombre': 'evento_prueba', 'username': 'evento_prueba', 'correo': 'prueba@gmail.com', 
         'password': '1234'}
     requests.post(url + 'CreateUsuario/', data=auth_data_us)
@@ -52,7 +88,14 @@ def test_eventos(url):
         print("\t -> CREACION DE UN EVENTO --> FUNCIONA")
     else:
         print("\t -> CREACION DE UN EVENTO --> NO FUNCIONA")
-    #Modificacion
+    # Creacion Mala
+    resp = requests.post(url + 'CreateEvento/', data=auth_data_ev)
+    t = resp.json()
+    if "Ya existe un evento con ese nombre" in t['message']:
+        print("\t -> NO CREACION DE UN EVENTO YA EXISTENTE --> FUNCIONA")
+    else:
+        print("\t -> NO CREACION DE UN EVENTO YA EXISTENTE --> NO FUNCIONA")
+    # Modificacion Buena
     auth_data_ev = {'nombre': 'evento_prueba', 'descripcion': 'evento_prueba1', 'fecha': '13/12/12', 
         'hora': '13', 'aforo': 13, 'categoria': 'evento_prueba1', 'organizador': 'evento_prueba'}
     resp = requests.put(url + 'UpdateEvento/evento_prueba/', data=auth_data_ev)
@@ -62,7 +105,7 @@ def test_eventos(url):
         print("\t -> MODIFICACION DE UN EVENTO--> FUNCIONA")
     else:
         print("\t -> MODIFICACION DE UN EVENTO --> NO FUNCIONA")
-    #Eliminacion
+    # Eliminacion Buena
     requests.delete(url + 'DeleteEvento/evento_prueba')
     resp = requests.get(url + 'Evento/evento_prueba')
     t = resp.json()
@@ -70,6 +113,14 @@ def test_eventos(url):
         print("\t -> ELIMINACION DE UN USUARIO --> FUNCIONA")
     else:
         print("\t -> ELIMINACION DE UN USUARIO --> NO FUNCIONA")
+    requests.delete(url + 'DeleteUsuario/evento_prueba')
+    # Eliminacion Mala
+    res = requests.delete(url + 'DeleteEvento/evento_prueba2')
+    t = resp.json()
+    if "Not found." in t['detail']:
+        print("\t -> ELIMINACION DE UN USUARIO NO EXISTENTE --> FUNCIONA")
+    else:
+        print("\t -> ELIMINACION DE UN USUARIO NO EXISTENTE --> NO FUNCIONA")
     requests.delete(url + 'DeleteUsuario/evento_prueba')
     print("-----------------------------------------------------\n")
 
@@ -82,13 +133,34 @@ def test_participantes(url):
     auth_data_ev = {'nombre': 'evento_prueba', 'descripcion': 'evento_prueba', 'fecha': '12/12/12', 
         'hora': '12', 'aforo': 12, 'categoria': 'evento_prueba', 'organizador': 'user_prueba'}
     requests.post(url + 'CreateEvento/', data=auth_data_ev)
-    # AÑADIR PARTICIPANTE
+    # AÑADIR PARTICIPANTE BIEN
     resp = requests.get(url + 'anadirParticipante/evento_prueba/user_prueba/')
     t = resp.json()
     if "correctamente" in t['message']:
         print("\t -> AÑADIR PARTICIPANTE --> FUNCIONA")
     else:
         print("\t -> AÑADIR PARTICIPANTE --> NO FUNCIONA")
+    # AÑADIR PARTICIPANTE, NO EXISTE USUAIRO
+    resp = requests.get(url + 'anadirParticipante/evento_prueba/user_prueba2/')
+    t = resp.json()
+    if "ERROR: No existe el usuario" in t['message']:
+        print("\t\t -> NO AÑADIR PARTICIPANTE, NO EXISTE USUARIO --> FUNCIONA")
+    else:
+        print("\t\t -> NO AÑADIR PARTICIPANTE, NO EXISTE USUARIO --> NO FUNCIONA")
+    # AÑADIR PARTICIPANTE, NO EXISTE EVENTO
+    resp = requests.get(url + 'anadirParticipante/evento_prueba1/user_prueba/')
+    t = resp.json()
+    if "ERROR: No existe el evento" in t['message']:
+        print("\t\t -> NO AÑADIR PARTICIPANTE, NO EXISTE EVENTO --> FUNCIONA")
+    else:
+        print("\t\t -> NO AÑADIR PARTICIPANTE, NO EXISTE EVENTO --> NO FUNCIONA")
+    # AÑADIR PARTICIPANTE, YA ES PARTICIPANTE
+    resp = requests.get(url + 'anadirParticipante/evento_prueba/user_prueba/')
+    t = resp.json()
+    if "ERROR: No se ha introducido o el usuario ya es participante" in t['message']:
+        print("\t\t -> NO AÑADIR PARTICIPANTE, YA ES PARTICIPANTE --> FUNCIONA")
+    else:
+        print("\t\t -> NO AÑADIR PARTICIPANTE, YA ES PARTICIPANTE --> NO FUNCIONA")
     # COMPROBAR PARTICIPANTE
     resp = requests.get(url + 'esParticipante/evento_prueba/user_prueba/')
     t = resp.json()
@@ -107,6 +179,7 @@ def test_participantes(url):
     requests.delete(url + 'DeleteUsuario/user_prueba')
     print("-----------------------------------------------------\n")
 
+# TEST MENSAJES
 def test_mensajes(url):
     print("-----------------------------------------------------\nTEST: MENSAJES")
     auth_data_us = {'nombre': 'user_prueba', 'username': 'user_prueba', 'correo': 'prueba@gmail.com', 
@@ -131,7 +204,7 @@ def test_mensajes(url):
         
 if __name__ == "__main__":
     pruebas = url_prueba
-    test_usuarios(pruebas)
-    test_eventos(pruebas)
+    # test_usuarios(pruebas)
+    # test_eventos(pruebas)
     test_participantes(pruebas)
-    test_mensajes(pruebas)
+    # test_mensajes(pruebas)
